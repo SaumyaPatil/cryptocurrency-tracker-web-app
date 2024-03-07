2468791e548a6d0e5f532cb6b7064dbfc27ea01e
SaumyaPatil <saupatil10dec@gmail.com>
2024-03-07 14:26:51 +0530

    Getting started with the project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <div className="nav-bar">
+        {/* Nav bar */}
+
+      </div>
+      <div className="page">
+        <span className='left'>
+
+          {/* Bitcoin chart */}
+
+          {/* Fundamentals */}
+
+          {/* Sentiments */}
+
+          {/* News Insights */}
+
+          {/* Tokenomics */}
+
+          {/* Team */}
+
+        </span>
+        <span className='right'>
+          {/* Advertisement */}
+
+          {/* Trending Coins */}
+
+        </span>
+      </div>
+      <div className="footer-section">
+        {/* Techicals */}
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
